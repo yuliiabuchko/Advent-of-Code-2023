7bc9c79cb7b14198e4b16c6977bac5658eaae243
Yuliia Buchko <yuliia.buchko@affirm.com>
2024-10-07 11:21:45 +0200

    Pylint day 02

--- day02/part1.py
+++ day02/part1.py
@@ -1,15 +1,20 @@
+"""Module provides solution for part 1"""
+
 from day02.parser import Game, GameEntry
 
 
 def is_game_entry_possible(game_entry: GameEntry) -> bool:
+    """Check if game entry is possible"""
     return game_entry.red <= 12 and game_entry.green <= 13 and game_entry.blue <= 14
 
 
 def is_game_possible(game: Game) -> bool:
+    """Check if all entries of the game are possible"""
     return all(is_game_entry_possible(game_entry) for game_entry in game.entries)
 
 
 def part1(games: list[Game]) -> int:
+    """Solve part 1"""
     res = 0
     for game in games:
         if is_game_possible(game):
--- day02/part2.py
+++ day02/part2.py
@@ -1,7 +1,10 @@
+"""Module provides solution for part 2"""
+
 from day02.parser import Game
 
 
 def part2(games: list[Game]) -> int:
+    """Solve part 2"""
     res = 0
     for game in games:
         max_red = max(entry.red for entry in game.entries)
