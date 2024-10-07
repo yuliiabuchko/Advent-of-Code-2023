09db236bb5b1d3ced644bd1edc4d56263b65c40f
Yuliia Buchko <yuliia.buchko@affirm.com>
2024-10-07 14:13:16 +0200

    Pylint day 03

--- day03/part2.py
+++ day03/part2.py
@@ -2,50 +2,40 @@
 
 from typing import Optional
 
-from day03.part1 import add_dots_to_input
+from day03.part1 import solve
 
 GEAR = '*'
 
 
 def part2(engine: list[list[str]]) -> int:
     """Solve part 2"""
-    n = len(engine)
-    m = len(engine[0])
-
-    engine = add_dots_to_input(engine)
-    i = 1
-
-    gears: dict[tuple[int, int], list[str]] = {}
-
-    while i <= n:
-        j = 1
-        curr_num = ''
-        start = None
-        while j <= m + 1:
-            if engine[i][j].isnumeric():
-                curr_num += engine[i][j]
-                if start is None:
-                    start = j
-            elif curr_num:
-                assert start is not None
-                gear = surrounded_gear(engine, i, start, j)
-                if gear is not None:
-                    if gear not in gears:
-                        gears[gear] = []
-                    gears[gear].append(curr_num)
-                curr_num = ''
-                start = None
-            j += 1
-        i += 1
+    gears = solve(engine, {}, is_surrounded_by_gear, add_to_gears)
 
     total = 0
-    for gear, values in gears.items():
+    for values in gears.values():
         if len(values) == 2:
             total += int(values[0]) * int(values[1])
     return total
 
 
-def surrounded_gear(engine: list[list[str]], i: int, j_start: int, j_end: int)\
+def is_surrounded_by_gear(engine: list[list[str]], i: int, start: int, j: int) \
+        -> tuple[bool, Optional[tuple[int, int]]]:
+    """Check if number is surrounded by gear, if so - return its coordinates"""
+    gear = surrounded_gear(engine, i, start, j)
+    return gear is not None, gear
+
+
+def add_to_gears(curr_num: str, gear: tuple[int, int],
+                 gears: dict[tuple[int, int], list[str]]) \
+        -> dict[tuple[int, int], list[str]]:
+    """Add `curr_num` to gears dict"""
+    if gear not in gears:
+        gears[gear] = []
+    gears[gear].append(curr_num)
+    return gears
+
+
+def surrounded_gear(engine: list[list[str]], i: int, j_start: int, j_end: int) \
         -> Optional[tuple[int, int]]:
     """Check if number is surrounded by gear, if so - return its coordinates"""
     if engine[i][j_start - 1] == GEAR:
